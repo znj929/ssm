db2fcda82de01f7796690028bea5ff160441c12a
znj <18379183218@163.com>
2019-04-07 14:30:44 +0800

    v0.12

--- src/main/java/com/hand/controller/HelloController.java
+++ src/main/java/com/hand/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 	@RequestMapping("/hello")
 	//@ResponseBody
 	public String Hello(){
-		String a= "a --merge v0.10 -reking github";
+		String a= "a --merge v0.10 -reking github v0.12";
 		return "index";
 	}
 }
